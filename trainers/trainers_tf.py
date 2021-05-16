dca70ec72d070029f27ba6bf811ae4c1b44e7b82
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-05-16 17:43:18 +0200

    trainers_tf improved

--- trainers/trainers_tf.py
+++ trainers/trainers_tf.py
@@ -2,15 +2,23 @@ import os
 import glob
 
 
-def airon_trainer(model, x_train, y_train, x_val, y_val, train_specs, mode, path,
-            verbose, batch_size, callbacks=None):
+def airon_trainer(model, x_train, y_train, x_val, y_val, epochs, verbose=0, callbacks=None, **train_specs):
 
-    best_model_name = path + 'best_epoch_model_' + mode
+    class_weight = {output_name: train_specs['class_weight'] for output_name in model.output_names} \
+        if 'class_weight' not in train_specs else None
+    mode = train_specs['mode'] if 'mode' in train_specs else None
+    path = train_specs['path'] if 'path' in train_specs else None
+    batch_size = train_specs['batch_size'] if 'batch_size' in train_specs else 32
+
+    best_model_name = None
 
     # Callbacks
     callbacks_ = []
     if callbacks:
         for callback_dict in callbacks:
+            if callback_dict['name'] == 'ModelCheckpoint':
+                ext = '_' + mode if mode else ''
+                best_model_name = path + 'best_epoch_model' + ext
             callbacks_ += [callback_dict['callback'](callback_dict['kargs'])]
         best_model_files = glob.glob(best_model_name + '*')
         if len(best_model_files) > 0:
@@ -18,11 +26,9 @@ def airon_trainer(model, x_train, y_train, x_val, y_val, train_specs, mode, path
                 os.remove(filename)
 
     # Train model
-    class_weight = None if 'class_weight' not in train_specs.keys() \
-        else {output_name: train_specs['class_weight'] for output_name in model.output_names}
     kargs = {'x': x_train,
              'y': y_train,
-             'epochs': train_specs['epochs'],
+             'epochs': epochs,
              'callbacks': callbacks_,
              'class_weight': class_weight,
              'shuffle': True,
