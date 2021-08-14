5d11844853530790d0db88945be77af8b100b292
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-08-14 12:45:54 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version='0.1.5',
+    version='0.1.6',
     scripts=[],
     author='Claudi Ruiz Camps',
     author_email='claudi_ruiz@hotmail.com',
