6dd00b8a1b7811c47700785c7767e1062aa6c6af
claudi.ruiz.camps <claudi.ruiz.camps@gmail.com>
2021-09-20 21:47:44 +0200

    refactoring

--- examples_torch/simplest_mnist_torch.py
+++ examples_torch/simplest_mnist_torch.py
@@ -4,7 +4,6 @@ import torch
 from torch.nn import functional as F
 from torch import nn
 from pytorch_lightning.core.lightning import LightningModule
-from sklearn.preprocessing import OneHotEncoder
 import torch.optim as optim
 import torchvision
 import os
