8d1f247af20f19406ce7c307f011c1d49515a9c4
claudi <claudi.ruiz.camps@gmail.com>
2021-11-25 20:33:34 +0100

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name=PACKAGE_NAME,
-    version='0.1.11',
+    version='0.1.12',
     scripts=[],
     author='Claudi Ruiz Camps',
     author_email='claudi_ruiz@hotmail.com',
@@ -28,7 +28,7 @@ setuptools.setup(
         'hyperopt==0.2.7',
         'tensorflow==2.7.0',
         'tensorboard==2.7.0',
-        'airontools==0.1.11'
+        'airontools==0.1.12'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
