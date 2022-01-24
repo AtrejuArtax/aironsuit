57f395baf749a41ce8bd51d6f430cb5285abe506
claudi <claudi.ruiz.camps@gmail.com>
2022-01-24 17:43:58 +0100

    always datasets

--- aironsuit/trainers.py
+++ aironsuit/trainers.py
@@ -67,16 +67,19 @@ def fit(module, x_train, y_train=None, x_val=None, y_val=None, sample_weight=Non
         training_kwargs.update({'validation_data': tuple(val_data)})
     if all([isinstance(data, tf.data.Dataset) for data in training_args]):
         # ToDo: make use of tfrecords for validation data too
-        training_kwargs['validation_data'] = \
-            tuple([tf.convert_to_tensor(list(val_data_.as_numpy_iterator()))
-                   for val_data_ in training_kwargs['validation_data']])
+        # training_kwargs['validation_data'] = \
+        #     tuple([tf.convert_to_tensor(list(val_data_.as_numpy_iterator()))
+        #            for val_data_ in training_kwargs['validation_data']])
+        training_kwargs['validation_data'] = tf.data.Dataset.zip(training_kwargs['validation_data'])
+        training_kwargs['validation_data'] = training_kwargs['validation_data'].batch(kwargs['batch_size'])
         if sample_weight is not None:
             training_args += [training_kwargs['sample_weight']]
             del training_kwargs['sample_weight']
-            training_args = tf.data.Dataset.from_tensor_slices(
-                tuple([list(train_data_.as_numpy_iterator()) for train_data_ in training_args]))
-        else:
-            training_args = tf.data.Dataset.zip(tuple(training_args))
+        #     training_args = tf.data.Dataset.from_tensor_slices(
+        #         tuple([list(train_data_.as_numpy_iterator()) for train_data_ in training_args]))
+        # else:
+        #     training_args = tf.data.Dataset.zip(tuple(training_args))
+        training_args = tf.data.Dataset.zip(tuple(training_args))
         training_args = training_args.batch(kwargs['batch_size'])
         module.fit(training_args, **training_kwargs)
     else:
