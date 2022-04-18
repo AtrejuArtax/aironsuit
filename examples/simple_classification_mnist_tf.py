922bf75d2416b04d5e83e79cb259f952b29f64cb
claudi <claudi_ruiz@hotmail.com>
2022-04-18 15:23:36 +0200

    bug fix

--- examples/simple_classification_mnist_tf.py
+++ examples/simple_classification_mnist_tf.py
@@ -128,7 +128,7 @@ aironsuit.summary()
 # COMMAND ----------
 
 # Evaluate
-score = aironsuit.evaluate(x_test, y_test)
+score = aironsuit.model.evaluate(x_test, y_test)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
