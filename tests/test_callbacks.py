af3ebd57fa83dbfcccccce7d5e2eca2711c7c5a0
claudi <claudi_ruiz@hotmail.com>
2022-09-26 09:56:33 +0200

    test fix

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -2,4 +2,10 @@ from aironsuit.callbacks import get_basic_callbacks
 
 
 def test_get_basic_callbacks():
-    assert isinstance(get_basic_callbacks(), dict)
+    basic_callbacks = get_basic_callbacks()
+    assert isinstance(basic_callbacks, list)
+    for callback_dict in basic_callbacks:
+        assert isinstance(callback_dict, dict)
+        for callback_name, callback_sub_dict in callback_dict.items():
+            assert isinstance(callback_name, str)
+            assert isinstance(callback_dict[callback_name]["kwargs"], dict)
