87bbcfafb04bfa1d6a79fe2f292c8f2964fc0e0c
Claudi Ruiz Camps <claudi_ruiz@hotmail.com>
2023-05-20 15:23:57 +0100

    simple classification mnist example refactor

--- examples/simple_classification_mnist_tf.py
+++ examples/simple_classification_mnist_tf.py
@@ -6,10 +6,8 @@ from airontools.constructors.layers import layer_constructor
 from airontools.preprocessing import train_val_split
 from airontools.tools import path_management
 from hyperopt import Trials
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.utils import to_categorical
+import tensorflow as tf
+from typing import Tuple, List
 
 from aironsuit.design.utils import choice_hp
 from aironsuit.suit import AIronSuit
@@ -25,7 +23,7 @@ working_path = os.path.join(HOME, "airon", project_name)
 model_name = project_name + "_NN"
 num_classes = 10
 batch_size = 32
-epochs = 3
+epochs = 1
 patience = 3
 max_evals = 1
 precision = "float32"
@@ -38,17 +36,17 @@ path_management(working_path, modes=["rm", "make"])
 # COMMAND ----------
 
 # Load and preprocess data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = np.expand_dims(x_train.astype("float32") / 255, -1)
-x_test = np.expand_dims(x_test.astype("float32") / 255, -1)
-y_train = to_categorical(y_train, num_classes)
-y_test = to_categorical(y_test, num_classes)
+(train_dataset, train_target), (test_dataset, test_target) = tf.keras.datasets.mnist.load_data()
+train_dataset = np.expand_dims(train_dataset.astype("float32") / 255, -1)
+test_dataset = np.expand_dims(test_dataset.astype("float32") / 255, -1)
+n_features = np.prod(train_dataset.shape[1:])
+train_target = tf.keras.utils.to_categorical(train_target, num_classes)
+test_target = tf.keras.utils.to_categorical(test_target, num_classes)
 
 # Split data per parallel model
 x_train, x_val, y_train, y_val, train_val_inds = train_val_split(
-    input_data=x_train,
-    output_data=y_train,
-    return_tfrecord=True,
+    input_data=train_dataset,
+    output_data=train_target,
 )
 
 # COMMAND ----------
@@ -56,34 +54,36 @@ x_train, x_val, y_train, y_val, train_val_inds = train_val_split(
 # Classifier Model constructor
 
 model_specs = {
-    "input_shape": (28, 28, 1),
+    "input_shape": train_dataset.shape[1:],
     "loss": "categorical_crossentropy",
     "optimizer": "adam",
     "metrics": ["accuracy"],
 }
 
 
-def classifier_model_constructor(**kwargs):
-    inputs = Input(shape=kwargs["input_shape"])
+def classifier_model_constructor(
+        input_shape: Tuple[int],
+        kernel_size: int,
+        filters: int,
+        num_heads: int,
+        loss: str,
+        optimizer: str,
+        metrics: List[str],
+):
+    inputs = tf.keras.layers.Input(shape=input_shape)
     outputs = layer_constructor(
         x=inputs,
-        filters=kwargs["filters"],  # Number of filters used for the convolutional layer
-        kernel_size=(
-            kwargs["kernel_size"],
-            kwargs["kernel_size"],
-        ),  # Kernel size used for the convolutional layer
+        filters=filters,  # Number of filters used for the convolutional layer
+        kernel_size=kernel_size,  # Kernel size used for the convolutional layer
         strides=2,  # Strides used for the convolutional layer
         sequential_axis=-1,  # Channel axis, used to define the sequence for the self-attention layer
-        num_heads=kwargs[
-            "num_heads"
-        ],  # Self-attention heads applied after the convolutional layer
+        num_heads=num_heads,  # Self-attention heads applied after the convolutional layer
         units=10,  # Dense units applied after the self-attention layer
         activation="softmax",  # Output activation function
-        advanced_reg=True,
     )
-    model = Model(inputs=inputs, outputs=outputs)
+    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
     model.compile(
-        loss=kwargs["loss"], optimizer=kwargs["optimizer"], metrics=kwargs["metrics"]
+        loss=loss, optimizer=optimizer, metrics=metrics,
     )
 
     return model
@@ -131,7 +131,7 @@ aironsuit.summary()
 # COMMAND ----------
 
 # Evaluate
-score = aironsuit.model.evaluate(x_test, y_test)
+score = aironsuit.model.evaluate(test_dataset, test_target)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
 
@@ -167,6 +167,6 @@ aironsuit.train(
 # COMMAND ----------
 
 # Evaluate
-score = aironsuit.model.evaluate(x_test, y_test)
+score = aironsuit.model.evaluate(test_dataset, test_target)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
