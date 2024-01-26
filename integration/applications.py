f841efdbdcccd28e1b731ac1ed3b2b02ce798606
Claudi Ruiz <claudi@trebellar.com>
2024-01-26 13:46:27 +0100

    refactoring

--- integration/applications.py
+++ integration/applications.py
@@ -1,8 +1,6 @@
-import getopt
 import os
 import pathlib
 import shutil
-import sys
 import time
 
 from utils import test_application
@@ -13,26 +11,7 @@ REPOS_PATH = os.sep.join(
 APPLICATIONS = [[os.path.join("aironsuit", "examples"), ["tensorflow"]]]
 
 
-def packages_manager(packages, mode):
-    [os.system("pip {} {}".format(mode, package)) for package in packages]
-    time.sleep(10)
-
-
 def integration_test():
-    # Clear, build and install packages
-    for package_name in ["airontools", "aironsuit"]:
-        repository_path = os.path.join(REPOS_PATH, package_name) + os.sep
-        for name in ["dist", "build", package_name + ".egg-info"]:
-            name_path = os.path.join(repository_path, name)
-            if os.path.isdir(name_path):
-                shutil.rmtree(name_path)
-        os.system("cd {} && python setup.py bdist_wheel".format(repository_path))
-        build_name = os.listdir(os.path.join(repository_path, "dist"))[0]
-        for action, package_name_ in zip(
-            ["uninstall", "install"], [package_name, build_name]
-        ):
-            os.system("pip {} {}".format(action, package_name))
-
     # Test applications
     for app_name, app_backends in APPLICATIONS:
         test_application(REPOS_PATH, app_name)
